d5707d86f22c671d1453cbba6d939d71a438b0ac
Philippe117 <15834849+Philippe117@users.noreply.github.com>
2018-01-22 19:19:42 -0500

    publie les erreurs sur le terminal principal

--- sh_files/Sara_total_bringup.sh
+++ sh_files/Sara_total_bringup.sh
@@ -40,24 +40,24 @@ shift $(( OPTIND - 1 ))
 if ! $HELP
 then
 
-	gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roscore ; echo 'roscore just died' >> /dev/stderr"
+	gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roscore ; echo 'roscore just died' >> $(tty)"
 	sleep 3
 
 	echo "Bringup the hardware"
-	gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roslaunch sara_launch sara_bringup.launch; echo 'Bringup just died' >> /dev/stderr"
+	gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roslaunch sara_launch sara_bringup.launch; echo 'Bringup just died' >> $(tty)"
 
 	echo "Launching Wonderland"
-	gnome-terminal --profile=Bringup -x bash -c "cd ~/sara_ws/wonderland/ ; python manage.py runserver; echo 'wonderland just died' >> /dev/stderr"
+	gnome-terminal --profile=Bringup -x bash -c "cd ~/sara_ws/wonderland/ ; python manage.py runserver; echo 'wonderland just died' >> $(tty)"
 
 
 	if $SPEECH
 	then
 		echo "Launching google speech to text"
-		gnome-terminal --profile=Voice -x bash -c "cd ~ ; source .bashrc ; roslaunch lab_ros_speech_to_text google_tts.launch; echo 'lab_ros_stt just died' >> /dev/stderr"
+		gnome-terminal --profile=Voice -x bash -c "cd ~ ; source .bashrc ; roslaunch lab_ros_speech_to_text google_tts.launch; echo 'lab_ros_stt just died' >> $(tty)"
 		echo "Launching speech splitter"
-		gnome-terminal --profile=Voice -x bash -c "source .bashrc ; roslaunch wm_speech_splitter sara_speech.launch; echo 'Speech splitter just died' >> /dev/stderr"
+		gnome-terminal --profile=Voice -x bash -c "source .bashrc ; roslaunch wm_speech_splitter sara_speech.launch; echo 'Speech splitter just died' >> $(tty)"
 		echo "Launching lu4r"
-		gnome-terminal --profile=Voice -x bash -c "cd ~/lu4r-0.2.1/lu4r-0.2.1/ ; java -Xmx1G -jar lu4r-server-0.2.1.jar simple amr en 9001; echo 'lu4r just died' >> /dev/stderr"
+		gnome-terminal --profile=Voice -x bash -c "cd ~/lu4r-0.2.1/lu4r-0.2.1/ ; java -Xmx1G -jar lu4r-server-0.2.1.jar simple amr en 9001; echo 'lu4r just died' >> $(tty)"
 		# echo "echo sara_command"
 		# gnome-terminal --profile=Voice -x bash -c "source .bashrc ; rostopic echo /sara_command"
 	fi
@@ -65,34 +65,34 @@ then
 	if $VISION
 	then
 		echo "Launching darknet"
-		gnome-terminal --profile=Vision -x bash -c "source .bashrc ; roslaunch darknet_ros darknet_ros.launch; echo 'darknet just died' >> /dev/stderr"
+		gnome-terminal --profile=Vision -x bash -c "source .bashrc ; roslaunch darknet_ros darknet_ros.launch; echo 'darknet just died' >> $(tty)"
 		echo "Launching frame to box"
-		gnome-terminal --profile=Vision -x bash -c "source .bashrc ; roslaunch wm_frame_to_box wm_frame_to_box.launch; echo 'frame_to_box just died' >> /dev/stderr"
+		gnome-terminal --profile=Vision -x bash -c "source .bashrc ; roslaunch wm_frame_to_box wm_frame_to_box.launch; echo 'frame_to_box just died' >> $(tty)"
 	fi
 
 	if $NAV 
 	then
 		echo "Launching navigation"
-		gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roslaunch sara_navigation move_base_amcl.launch; echo 'move_base just died' >> /dev/stderr"
+		gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roslaunch sara_navigation move_base_amcl.launch; echo 'move_base just died' >> $(tty)"
 	fi
 
 
 	if $STATEMACHINE
 	then
 		echo "Launching flexbe core"
-		gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roslaunch flexbe_onboard behavior_onboard.launch; echo 'flexbe onboard just died' >> /dev/stderr"
+		gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roslaunch flexbe_onboard behavior_onboard.launch; echo 'flexbe onboard just died' >> $(tty)"
 		echo "Launching flexbe widget"
-		gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roslaunch flexbe_widget behavior_ocs.launch; echo 'flexbe widget just died' >> /dev/stderr"
+		gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; roslaunch flexbe_widget behavior_ocs.launch; echo 'flexbe widget just died' >> $(tty)"
 	fi
 
 	echo "Launching rviz"
-	gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; rviz; echo 'rviz just died' >> /dev/stderr"
+	gnome-terminal --profile=Bringup -x bash -c "source .bashrc ; rviz; echo 'rviz just died' >> $(tty)"
 
 	if $TELE
 	then
 		echo "Launching teleop"
-		gnome-terminal --profile=Vision -x bash -c "source .bashrc ; roslaunch sara_teleop sara_teleop.launch; echo 'teleop just died' >> /dev/stderr"
+		gnome-terminal --profile=Vision -x bash -c "source .bashrc ; roslaunch sara_teleop sara_teleop.launch; echo 'teleop just died' >> $(tty)"
 	fi
 
-	sleep 10
+	echo "Everything is running now."
 fi
