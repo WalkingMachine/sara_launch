e7e44593f1a311c84e17e2d123a94cbd28f079b0
philippe <philippe117@live.fr>
2019-03-23 16:59:19 -0400

    Ajuste le launchscript pour éviter de tuer les processes qui ne dépendes pas du hardware

--- sh_files/Sara_total_bringup.sh
+++ sh_files/Sara_total_bringup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 
+# Define process ID file names
+PIDFILENAMEPERSISTENT="tempPIDPersistent"
+PIDFILENAMEESTOP="tempPIDEStop"
+
+
 VISION=false
 SPEECH=false
 HELP=false
@@ -56,23 +61,31 @@ while getopts "asvhtfJngzl:mo" opt; do
 done
 shift $(( OPTIND - 1 ))
 
-
+function cleanup_all {
+    cleanup "$PIDFILENAMEESTOP"
+    cleanup "$PIDFILENAMEPERSISTENT"
+}
 
 # Function that can kill all orbital terminals
 function cleanup {
-    echo 'killing all processes'
-    for f in $(cat /tmp/tempPID)
+    FilePID=$1
+
+    echo "killing all processes on $FilePID"
+    for f in $(cat "/tmp/$FilePID")
     do
         kill -s 9 $f
     done
-    echo > /tmp/tempPID
+    echo > "/tmp/$FilePID"
+
 }
-trap cleanup EXIT
+trap cleanup_all EXIT
 
 
 function start_terminal_node {
     Name=$1
     Command=$2
+    PIDFile=$3
+
     echo "Starting $Name"
     SARACMD='( while true ; do setTitle '"$Name"' ; sleep 2 ; done ) & '
     SARACMD+=$Command
@@ -80,6 +93,7 @@ function start_terminal_node {
     SARACMD+='; echo -e "$(tput setaf 1)$(tput setab 7)Im dead"'
     gnome-terminal --hide-menubar --profile=SARA
 }
+
 
 function check_usb_devices {
     result=true
@@ -122,26 +136,67 @@ function check_usb_devices {
     fi
 }
 
+####################################################################
+#                            SETUP                                 #
+####################################################################
 
 # Export the variable that will contain the command for each orbital terminals
 export SARACMD
+export PIDFile
+
+
+
+####################################################################
+#                         MAIN PROGRAM                             #
+####################################################################
 
 if ! $HELP
 then
-    start_terminal_node "roscore" "roscore"
-    sleep 3
 
+    ####################################################
+    # Launch all persistent processes
+
+    echo > /tmp/tempPIDPersistent
+    start_terminal_node "roscore" "roscore" ""$PIDFILENAMEPERSISTENT""
+
+    sleep 3
 
     if ${FORCEOFFLINE}
         then
         rosparam set /force_offline true
     fi
 
-    start_terminal_node "SOUNDBOARD" 'roslaunch wm_sound_library wm_soundboard.launch' 
+    start_terminal_node "SOUNDBOARD" 'roslaunch wm_sound_library wm_soundboard.launch'  ""$PIDFILENAMEPERSISTENT""
+
     echo "Load categoryToNames.yaml"
     rosparam load ~/sara_ws/src/sara_launch/sh_files/ressources/categoryToNames.yaml CategoryToNames
     echo "Setting voice to $LANGUE"
     rosparam set /langue $LANGUE
+
+    start_terminal_node "MARY_TTS" "cd ~/sara_ws/src/marytts ; ./gradlew run"  ""$PIDFILENAMEPERSISTENT""
+
+
+
+    start_terminal_node "WONDERLAND" 'cd ~/sara_ws/src/wonderland/ ; python manage.py runserver 0.0.0.0:8000'  "$PIDFILENAMEPERSISTENT"
+
+    start_terminal_node "WONDERLAND_PUBLISHER" 'rosrun wonderland publish_objects_3D.py'  "$PIDFILENAMEPERSISTENT"
+
+    start_terminal_node "SARA_NLU" 'roslaunch wm_nlu wm_nlu.launch'  "$PIDFILENAMEPERSISTENT"
+
+    start_terminal_node "FLEXBE_ENGINE" 'roslaunch flexbe_onboard behavior_onboard.launch'  "$PIDFILENAMEPERSISTENT"
+
+    start_terminal_node "DTP_SERVICE" 'rosrun wm_direction_to_point direction_to_point_server.py'  "$PIDFILENAMEPERSISTENT"
+
+    start_terminal_node "SARA_WEB_INTERFACE" 'roslaunch sara_launch web_interface.launch'  "$PIDFILENAMEPERSISTENT"
+
+    start_terminal_node "FRAME_TO_BOX" 'roslaunch wm_frame_to_box wm_frame_to_box.launch'  "$PIDFILENAMEPERSISTENT"
+
+
+    if ${STATEMACHINE}
+    then
+        start_terminal_node "FLEXBE_APP" 'roslaunch flexbe_app flexbe_ocs.launch'  "$PIDFILENAMEPERSISTENT"
+    fi
+
 
 
 
@@ -156,7 +211,7 @@ then
         echo "====================="
             sleep 1
         done
-        echo > tempPID
+
 
         clear
         echo "======================="
@@ -164,17 +219,20 @@ then
         echo "======================="
         echo
 
+        echo > /tmp/"$PIDFILENAMEESTOP"
 
-        
 
-        
+
+
+
+        ####################################################
+        # Launch all hardware dependent processes
+
 
 #        rosparam set use_sim_time true
 #        date --set="$ssh nvidia@sara-jetson1"
 
-        start_terminal_node "MARY_TTS" "cd ~/sara_ws/src/marytts ; ./gradlew run"
-
-        start_terminal_node "SARA_HARDWARE" 'roslaunch sara_launch sara_bringup.launch'
+        start_terminal_node "SARA_HARDWARE" 'roslaunch sara_launch sara_bringup.launch'  "$PIDFILENAMEESTOP"
 
         sleep 1
 
@@ -183,29 +241,16 @@ then
             COMMAND='while [ ! $(ssh -t -t nvidia@sara-jetson1 "echo ok" ) ] ; do echo $(tput setaf 3)Still waiting for jetson$(tput setaf 7) ; sleep 1; done'
             COMMAND+="; ssh -t -t nvidia@sara-jetson1 'sudo service ntp stop; sudo ntpd -gq'"
             COMMAND+="; ssh -t -t nvidia@sara-jetson1 'cd /home/nvidia ; roslaunch sara_launch jetson.launch'"
-            start_terminal_node "JETSON" "$COMMAND"
+            start_terminal_node "JETSON" "$COMMAND"  "$PIDFILENAMEESTOP"
         fi
 
-#        start_terminal_node "SOUNDBOARD" ""
+#        start_terminal_node "SOUNDBOARD" ""  "$PIDFILENAMEESTOP"
 
         VIZ=$(rospack find vizbox)
-        start_terminal_node "VIZBOX" "cd $VIZ ; ./server.py"
+        start_terminal_node "VIZBOX" "cd $VIZ ; ./server.py"  "$PIDFILENAMEESTOP"
 
-        start_terminal_node "UI_HELPER" 'rosrun sara_ui sara_ui_helper'
+        start_terminal_node "UI_HELPER" 'rosrun sara_ui sara_ui_helper'  "$PIDFILENAMEESTOP"
 
-        start_terminal_node "WONDERLAND" 'cd ~/sara_ws/src/wonderland/ ; python manage.py runserver 0.0.0.0:8000'
-
-        start_terminal_node "WONDERLAND_PUBLISHER" 'rosrun wonderland publish_objects_3D.py'
-
-        start_terminal_node "SARA_NLU" 'roslaunch wm_nlu wm_nlu.launch'
-
-        start_terminal_node "FLEXBE_ENGINE" 'roslaunch flexbe_onboard behavior_onboard.launch'
-
-        start_terminal_node "DTP_SERVICE" 'rosrun wm_direction_to_point direction_to_point_server.py'
-
-        start_terminal_node "SARA_WEB_INTERFACE" 'roslaunch sara_launch web_interface.launch'
-
-        start_terminal_node "FRAME_TO_BOX" 'roslaunch wm_frame_to_box wm_frame_to_box.launch'
 
         sleep 2
 
@@ -216,34 +261,29 @@ rosservice call /wm_play_sound "play:
 
         if ${SPEECH}
         then
-            start_terminal_node "GOOGLE_STT" 'roslaunch lab_ros_speech_to_text google_tts.launch'
+            start_terminal_node "GOOGLE_STT" 'roslaunch lab_ros_speech_to_text google_tts.launch'  "$PIDFILENAMEESTOP"
 
-            start_terminal_node "SPEECH_SPLITTER" 'roslaunch wm_speech_splitter sara_speech.launch'
+            start_terminal_node "SPEECH_SPLITTER" 'roslaunch wm_speech_splitter sara_speech.launch'  "$PIDFILENAMEESTOP"
         fi
 
         if ${NAV}  && ! ${MAPPING}
         then
-            start_terminal_node "NAVIGATION" 'roslaunch sara_navigation move_base_amcl.launch'
+            start_terminal_node "NAVIGATION" 'roslaunch sara_navigation move_base_amcl.launch'  "$PIDFILENAMEESTOP"
         fi
 
         if ${MAPPING}
         then
-            start_terminal_node "SLAM_NAVIGATION" 'roslaunch sara_launch wm_slam_gmapping.launch'
-        fi
-
-        if ${STATEMACHINE}
-        then
-            start_terminal_node "FLEXBE_APP" 'roslaunch flexbe_app flexbe_ocs.launch'
+            start_terminal_node "SLAM_NAVIGATION" 'roslaunch sara_launch wm_slam_gmapping.launch'  "$PIDFILENAMEESTOP"
         fi
 
         if ${RVIZ}
         then
-            start_terminal_node "RVIZ" 'rviz'
+            start_terminal_node "RVIZ" 'rviz'  "$PIDFILENAMEESTOP"
         fi
 
         if ${TELEOP}
         then
-            start_terminal_node "SARA_TELEOP" 'roslaunch sara_teleop sara_teleop.launch'
+            start_terminal_node "SARA_TELEOP" 'roslaunch sara_teleop sara_teleop.launch'  "$PIDFILENAMEESTOP"
         fi
 
     sleep 2
@@ -251,13 +291,13 @@ rosservice call /wm_play_sound "play:
         if ${VISION}
         then
 
-            start_terminal_node "COLOR_DETECTOR" 'roslaunch wm_color_detector wm_color_detector.launch'
+            start_terminal_node "COLOR_DETECTOR" 'roslaunch wm_color_detector wm_color_detector.launch'  "$PIDFILENAMEESTOP"
 
-            start_terminal_node "LAPTOP_DARKNET" 'roslaunch darknet_ros darknet_ros.launch darknet_name:=darknet_ros_laptop'
+            start_terminal_node "LAPTOP_DARKNET" 'roslaunch darknet_ros darknet_ros.launch darknet_name:=darknet_ros_laptop'  "$PIDFILENAMEESTOP"
 
-            start_terminal_node "FACE_DETECTOR" 'roslaunch ros_face_recognition ros-face-recognition.launch'
+            start_terminal_node "FACE_DETECTOR" 'roslaunch ros_face_recognition ros-face-recognition.launch'  "$PIDFILENAMEESTOP"
 
-            start_terminal_node "DATA_COLLECTOR" 'roslaunch wm_data_collector data_collector.launch'
+            start_terminal_node "DATA_COLLECTOR" 'roslaunch wm_data_collector data_collector.launch'  "$PIDFILENAMEESTOP"
         fi
 
         echo "fully running"
@@ -270,7 +310,7 @@ rosservice call /wm_play_sound "play:
 
 
 
-        start_terminal_node "WM_TTS" 'roslaunch wm_tts wm_tts.launch'
+        start_terminal_node "WM_TTS" 'roslaunch wm_tts wm_tts.launch'  "$PIDFILENAMEESTOP"
 
 
 rostopic pub /say wm_tts/say "sentence: 'Walking Machine. Operationnal.'
@@ -281,10 +321,15 @@ emotion: 0" --once
         do
             sleep 1
         done
+
+
+
+        ####################################################
+        # Kill all hardware dependent processes
+
         echo '$(tput setaf 1)Sara is dead. For now...$(tput setaf 7)'
 
 
-        cleanup
+        cleanup "$PIDFILENAMEESTOP"
     done
-
 fi
