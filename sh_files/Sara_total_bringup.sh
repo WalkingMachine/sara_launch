dfdefd71200793e5e1639d20eb5f0c164e16b143
philippe <philippe117@live.fr>
2020-07-23 15:21:23 -0400

    Ajuste le launchfile pour fonctionner avec la nouvelle stack de sara_launch

--- sh_files/Sara_total_bringup.sh
+++ sh_files/Sara_total_bringup.sh
@@ -119,6 +119,11 @@ function check_usb_devices {
         echo "$(tput setaf 1)No connection to laser_base$(tput setaf 7)"
         result=false
     fi
+    if ! [ $(ssh -t -t nvidia@sara-jetson1 "echo ok" ) ]
+    then
+        echo "$(tput setaf 1)No connection to Jetson$(tput setaf 7)"
+        result=false
+    fi
     if [ -r /dev/SARA/motors/roboteq ]
     then
          if ! [ 4 -eq $(ls -1 /dev/SARA/motors/roboteq | wc -l) ]
@@ -172,32 +177,14 @@ then
         rosparam set /force_offline true
     fi
 
-    start_terminal_node "SOUNDBOARD" 'roslaunch wm_sound_library wm_soundboard.launch'  ""$PIDFILENAMEPERSISTENT""
-
     echo "Load categoryToNames.yaml"
     rosparam load ~/sara_ws/src/sara_launch/sh_files/ressources/categoryToNames.yaml CategoryToNames
     echo "Setting voice to $LANGUE"
     rosparam set /langue $LANGUE
 
+    start_terminal_node "SOUNDBOARD" 'roslaunch wm_sound_library wm_soundboard.launch'  ""$PIDFILENAMEPERSISTENT""
     start_terminal_node "MARY_TTS" "cd ~/sara_ws/src/marytts ; ./gradlew run"  ""$PIDFILENAMEPERSISTENT""
-
-
-
     start_terminal_node "WONDERLAND" 'cd ~/sara_ws/src/wonderland/ ; python manage.py runserver 0.0.0.0:8000'  "$PIDFILENAMEPERSISTENT"
-
-    start_terminal_node "WONDERLAND_PUBLISHER" 'rosrun wonderland publish_objects_3D.py'  "$PIDFILENAMEPERSISTENT"
-
-    start_terminal_node "SARA_NLU" 'roslaunch wm_nlu wm_nlu.launch'  "$PIDFILENAMEPERSISTENT"
-
-    start_terminal_node "DTP_SERVICE" 'rosrun wm_direction_to_point direction_to_point_server.py'  "$PIDFILENAMEPERSISTENT"
-
-    start_terminal_node "SARA_WEB_INTERFACE" 'roslaunch sara_launch web_interface.launch'  "$PIDFILENAMEPERSISTENT"
-
-    start_terminal_node "FRAME_TO_BOX" 'roslaunch wm_frame_to_box wm_frame_to_box.launch'  "$PIDFILENAMEPERSISTENT"
-
-    start_terminal_node "SARA_SAID_AND_EARD" 'rostopic echo /sara_command & rostopic echo /sara_said'  "$PIDFILENAMEPERSISTENT"
-
-
 
     if ${SPEECH}
     then
@@ -210,21 +197,12 @@ then
         fi
     fi
 
-
-    if ${STATEMACHINE}
-    then
-        start_terminal_node "FLEXBE" 'roslaunch flexbe_app flexbe_full.launch'  "$PIDFILENAMEPERSISTENT"
-    fi
-
-    if ${RVIZ}
-    then
-        start_terminal_node "RVIZ" 'rviz'  "$PIDFILENAMEPERSISTENT"
-    fi
-
     start_terminal_node "WM_TTS" 'roslaunch wm_tts wm_tts.launch'  "$PIDFILENAMEPERSISTENT"
 
-    sleep 3
+    start_terminal_node "SARA_SAID_AND_EARD" 'rostopic echo /sara_command & rostopic echo /sara_said'  "$PIDFILENAMEPERSISTENT"
 
+
+    sleep 2
     rostopic pub /say wm_tts/say "sentence: 'My brain is ready.'
 emotion: 0" --once
 
@@ -269,29 +247,14 @@ emotion: 0" --once
 #        rosparam set use_sim_time true
 #        date --set="$ssh nvidia@sara-jetson1"
 
+
         start_terminal_node "SARA_HARDWARE" 'roslaunch sara_launch sara_bringup.launch'  "$PIDFILENAMEESTOP"
 
-        sleep 2
-
-        start_terminal_node "SARA_MOVEIT" 'roslaunch sara_moveit move_group.launch'  "$PIDFILENAMEESTOP"
-
-        if ${JET}
-        then
-            COMMAND='while [ ! $(ssh -t -t nvidia@sara-jetson1 "echo ok" ) ] ; do echo $(tput setaf 3)Still waiting for jetson$(tput setaf 7) ; sleep 1; done'
-            COMMAND+="; ssh -t -t nvidia@sara-jetson1 'sudo service ntp stop; sudo ntpd -gq'"
-            COMMAND+="; ssh -t -t nvidia@sara-jetson1 'cd /home/nvidia ; roslaunch sara_launch sara_jetson.launch'"
-            start_terminal_node "JETSON" "$COMMAND"  "$PIDFILENAMEESTOP"
-        fi
-
-#        start_terminal_node "SOUNDBOARD" ""  "$PIDFILENAMEESTOP"
 
         VIZ=$(rospack find vizbox)
         start_terminal_node "VIZBOX" "cd $VIZ ; ./server.py"  "$PIDFILENAMEESTOP"
 
-        #start_terminal_node "UI_HELPER" 'rosrun sara_ui sara_ui_helper'  "$PIDFILENAMEESTOP"
-
-
-        sleep 4
+        sleep 2
 
 
 
@@ -299,49 +262,8 @@ rosservice call /wm_play_sound "play:
   data: 'sara-start.wav'"
 
 
-        if ${TELEOP}
-        then
-            start_terminal_node "SARA_TELEOP" 'roslaunch sara_teleop sara_teleop.launch'  "$PIDFILENAMEESTOP"
-        fi
-
     sleep 2
 
-        if ${NAV}  && ! ${MAPPING}
-        then
-            start_terminal_node "NAVIGATION" 'roslaunch sara_navigation move_base_amcl.launch'  "$PIDFILENAMEESTOP"
-        fi
-
-        if ${MAPPING}
-        then
-            start_terminal_node "SLAM_NAVIGATION" 'roslaunch sara_navigation wm_slam_gmapping.launch'  "$PIDFILENAMEESTOP"
-        fi
-
-
-        if ${VISION}
-        then
-
-            start_terminal_node "COLOR_DETECTOR" 'roslaunch wm_color_detector wm_color_detector.launch'  "$PIDFILENAMEESTOP"
-
-#            start_terminal_node "LAPTOP_DARKNET" 'roslaunch darknet_ros darknet_ros.launch darknet_name:=darknet_ros_laptop'  "$PIDFILENAMEESTOP"
-
-            start_terminal_node "FACE_DETECTOR" 'roslaunch ros_face_recognition ros-face-recognition.launch'  "$PIDFILENAMEESTOP"
-
-            start_terminal_node "ENTITY_TRACKER" 'roslaunch wm_entity_tracker wm_entity_tracker.launch'  "$PIDFILENAMEESTOP"
-
-            start_terminal_node "OBJECT_SEGMENTATION" 'roslaunch wm_object_segmentation wm_object_segmentation.launch'  "$PIDFILENAMEESTOP"
-
-            start_terminal_node "GRASP_DETECTION" 'roslaunch gpd tutorial2.launch'  "$PIDFILENAMEESTOP"
-
-            start_terminal_node "OPENPIFPAF_SERVER" 'python3.7 -m openpifpafwebdemo.server'  "$PIDFILENAMEESTOP"
-
-            start_terminal_node "OPENPIFPAF_WRAPPER" 'sleep 10 ; rosrun wm_openpifpaf_ros_wrapper test_pose.py'  "$PIDFILENAMEESTOP"
-
-
-
-rosparam set /process_object_segmentation false
-rosparam set /process_table_segmentation false
-
-        fi
 
         echo "fully running"
         echo "Open this link to open vizbox:"
